849066bef6fb81e5d84e1f78185ba7208c93b19e
Dmitriy <semenovsdo@mail.ru>
2023-01-31 23:48:33 +0300

    Example 008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,21 @@
+﻿int distanse = 10000;
+int a = 3;
+int b = 2;
+int d = 10;
+int count = 0;
+while (distanse > 10)
+{
+    int time = 0;
+    if (count % 2 == 0)
+    {
+        time = distanse / (a + d);
+        distanse = (a + b) * time;
+    }
+    if (count % 2 > 0)
+    {
+        time = distanse / (b + d);
+        distanse = (a + b) * time;
+    }
+    count++;
+}
+Console.WriteLine(count);
