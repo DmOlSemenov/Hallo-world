b85ec49b55a76e44a2846e6e19983c1fcbd21e46
Dmitriy <semenovsdo@mail.ru>
2023-02-02 23:01:35 +0300

    ArrayMethod

---
+++ Example011_ArrayMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {19, 26, 31, 64, 519, 6, 71, 7, 29};
+int n = array.Length;
+int find = 71;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+        
+    }
+    //index = index + 1
+    index++;
+}
+
